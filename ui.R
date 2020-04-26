77024a586ca9b39147567dd68320a5efecd1cac9
vinayprakashanalytics808 <vinayprakash.analytics808@gmail.com>
2020-04-26 04:04:47 -0700

    Changed UI

--- ui.R
+++ ui.R
@@ -9,14 +9,8 @@ library(dplyr)
 library(tidyverse)
 
 # Define UI for application that draws a histogram
-navbarPage("Trading Analysis",position = "fixed-top",
-           selected = "Data", 
-           tags$script(src = "app.js"),
-           tags$style("body {background-color: skyblue;}"),
-           tags$head(tags$style(".shiny-output-error{visibility: hidden;}")),
-           tags$style(type = "text/css", "body{padding-top: 65px; padding-left:0px;}"),
-           tags$head(tags$style(HTML('.navbar-brand{width: 200px; font-size:20px; text-align:center;}'))),
-           tabPanel("Data",
+navbarPage("Trading Analysis",position = "fixed-top",fluid = TRUE,id = "Main",
+           tabPanel("Data", 
                     sidebarLayout(
                       sidebarPanel(selectInput('ticker','Ticker',choices = c("",levels(factor(Ticker$Ticker))),selected = ""),
                                    # uiOutput("measures"),
@@ -26,6 +20,12 @@ navbarPage("Trading Analysis",position = "fixed-top",
                       mainPanel(dataTableOutput("display"),
                                 br(),br(),br(),br(),
                                 amChartsOutput("ts"))
-                    )))
+                    )),
+           tags$style(".Main{padding-top:100px;}"),
+           tags$style("body {background-color: skyblue;}"),
+           tags$style("#display tr{background-color: #4d0000;}","#display td, th{color: white;}"),
+           tags$style("body{padding-top: 65px; padding-left:0px;}"),
+           tags$head(tags$style(".shiny-output-error{visibility: hidden;}")))
+
 
 
