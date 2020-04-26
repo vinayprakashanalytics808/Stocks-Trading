3868c1cca9eeee941c8271189bf5b2a8105e3521
vinayprakashanalytics808 <vinayprakash.analytics808@gmail.com>
2020-04-26 04:07:51 -0700

    Changed the location of the excel file

--- data_wrangling.R
+++ data_wrangling.R
@@ -14,6 +14,7 @@
 #' 
 
 library(readxl)
-Ticker <- read_excel("C:/Users/admin/Documents/R shiny/Stocks-Trading/Ticker.xlsx")
+# Ticker <- read_excel("C:/Users/admin/Documents/R shiny/Stocks-Trading/Ticker.xlsx")
+Ticker <- read_excel("Ticker.xlsx")
 Ticker <- Ticker %>% filter(!str_detect(Ticker, ".BO"))
 Ticker <- as.data.frame(Ticker)
